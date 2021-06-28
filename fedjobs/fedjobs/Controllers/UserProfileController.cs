c1a749e060f4cf4a7d65509065d64e80d2f6c410
Chance Lane <lanecw@gmail.com>
2021-06-28 15:24:53 -0400

    file buildout

---
+++ fedjobs/fedjobs/Controllers/UserProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using fedjobs.Models;
+using fedjobs.Repositories;
+
+namespace fedjobs.Controllers
+{
+    /55432389 / Use the Authorize tag to require user authorization to any of the controller methods
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileRepository _userProfileRepository;
+        //private readonly IPostRepository _postRepository;
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+            //_postRepository = postRepository;
+        }
+
+        [HttpGet("{firebaseUserId}")]
+        public IActionResult GetUserProfile(string firebaseUserId)
+        {
+            return Ok(_userProfileRepository.GetByFirebaseUserId(firebaseUserId));
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserProfile userProfile)
+        {
+            userProfile.CreateDateTime = DateTime.Now;
+            userProfile.UserTypeId = UserType.AUTHOR_ID;
+            _userProfileRepository.Add(userProfile);
+            return CreatedAtAction(
+                nameof(GetUserProfile),
+                new { firebaseUserId = userProfile.FirebaseUserId },
+                userProfile);
+        }
+    }
+}
