ce590267ac4386c58ffbbca9da25524e0472556b
Chance Lane <lanecw@gmail.com>
2021-07-01 11:21:18 -0400

    search login requires one search parameter to be defined

--- fedjobs/fedjobs/client/src/components/SearchBar.js
+++ fedjobs/fedjobs/client/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useContext } from "react";
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input,  } from 'reactstrap';
 import { useHistory, Link } from "react-router-dom";
 import { JobDataContext } from "../providers/JobsDataProvider";
 
@@ -12,10 +12,15 @@ export default function SearchBar() {
 
   const submitSearch = (e) => {
     e.preventDefault();
-    if (!keyword && !location){
-      console.log('nonnonno');
+
+    //the search will not occur unless a keyword or location is defined(see onSubmit in jsx. If the search is to occur however, and one of the fields is not defined, the search will be hindered. This ensures that if 1 field is not defined, the search will occur with an empty string for best results)
+    if (!keyword){
+      getJobs(' ', location)
     }
-    getJobs(keyword, location)
+    if (!location){
+      getJobs(keyword, ' ')
+    }
+    
   };
 
   
@@ -23,7 +28,9 @@ export default function SearchBar() {
 
 
 return (
-    <Form onSubmit={submitSearch}>
+    <Form onSubmit={(!keyword || keyword === ' ') && (!location || location === ' ') ?
+    console.log('Please select a Keyword or location to search'):
+    submitSearch}>
       <fieldset>
         <FormGroup>
           <Label for="keyword">Job Title or Keyword</Label>
